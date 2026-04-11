66820670f8c26b962fb0677cf98cfa007532e8f5
000926085 <000926085@mohawkcollege.ca>
2026-04-10 22:36:04 -0400

    eslint imports fix.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
